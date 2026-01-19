68a7fbed10d051d0bbd099ac52739cf2344468ee
Etienne Démoulin <etiennedemoulin@MacBook-Pro-Etienne-2.local>
2026-01-19 16:09:55 +0100

    test v3

---
+++ v3/basicTest3_0_1.js
@@ -0,0 +1,34 @@
+import { Radio, RtlProvider } from "@jtarrio/webrtlsdr/radio.js"; 
+import { Demodulator } from "@jtarrio/signals/demod/demodulator.js";
+import { webusb } from "usb"; 
+import { AudioContext } from 'isomorphic-web-audio-api';
+globalThis.AudioContext = AudioContext;
+globalThis.navigator.usb = webusb;
+
+
+let demodulator = new Demodulator({
+	modeOption: {
+  /**
+   * The time constant for the deemphasizer, in microseconds. 50 by default.
+   *
+   * This should be 75 for the US and South Korea, 50 everywhere else.
+   */
+  deemphasizerTc: 50,
+  /** Number of taps for the downsampler filter. Must be an odd number. 151 by default. */
+  downsamplerTaps: 151,
+  /** Number of taps for the RF filter. Must be an odd number. 151 by default. */
+  rfTaps: 151,
+  /** Number of taps for the audio filter. Must be an odd number. 41 by default. */
+  audioTaps: 41
+},
+});
+
+
+
+let radio = new Radio(new RtlProvider(), demodulator, { bufferPerSecond : 20 });  // by default buffersPerSecond : 20 (histoires de latence)
+radio.setFrequency(91.7e6);
+demodulator.setVolume(1);
+radio.start();
+
+
+// essayer avec la vraie antenne, sans globalThis, puis sur la raspberry en changeant le nombre de tappsssss
