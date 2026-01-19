51629874f440d7ce61ac1857d3542f16f7720a40
Etienne Démoulin <etiennedemoulin@MacBook-Pro-Etienne-2.local>
2026-01-19 18:13:19 +0100

    adding audio examples

--- v2/basicTest2_0_5.js
+++ v2/basicTest2_0_5.js
@@ -8,25 +8,26 @@ globalThis.navigator.usb = webusb;
 
 
 let demodulator = new Demodulator({
-	modeOption: {
-  /**
-   * The time constant for the deemphasizer, in microseconds. 50 by default.
-   *
-   * This should be 75 for the US and South Korea, 50 everywhere else.
-   */
-  deemphasizerTc: 50,
-  /** Number of taps for the downsampler filter. Must be an odd number. 151 by default. */
-  downsamplerTaps: 151,
-  /** Number of taps for the RF filter. Must be an odd number. 151 by default. */
-  rfTaps: 151,
-  /** Number of taps for the audio filter. Must be an odd number. 41 by default. */
-  audioTaps: 41
-},
+    modeOptions: {
+        "WBFM": {
+          /**
+           * The time constant for the deemphasizer, in microseconds. 50 by default.
+           *
+           * This should be 75 for the US and South Korea, 50 everywhere else.
+           */
+          deemphasizerTc: 50,
+          /** Number of taps for the downsampler filter. Must be an odd number. 151 by default. */
+          downsamplerTaps: 151,
+          /** Number of taps for the RF filter. Must be an odd number. 151 by default. */
+          rfTaps: 151,
+          /** Number of taps for the audio filter. Must be an odd number. 41 by default. */
+          audioTaps: 41
+        }
+    }
 });
 
 
-
-let radio = new Radio(new RTL2832U_Provider(), demodulator, {buffersPerSecond:60}); // by default buffersPerSecond : 20 (histoires de latence)
+let radio = new Radio(new RTL2832U_Provider(), demodulator, {buffersPerSecond:20}); // by default buffersPerSecond : 20 (histoires de latence)
 // let radio = new Radio(new RtlProvider(), demodulator);
 radio.setFrequency(91.7e6);
 demodulator.setVolume(1);
