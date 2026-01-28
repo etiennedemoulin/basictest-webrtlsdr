a9541c0345f5303920d7c524ce64b07aa53fe969
Etienne Démoulin <etiennedemoulin@MacBook-Pro-Etienne-2.local>
2026-01-28 15:06:32 +0100

    testing mono/stereo

--- v2/basicTest2_0_5.js
+++ v2/basicTest2_0_5.js
@@ -1,4 +1,5 @@
 import { Radio } from "@jtarrio/webrtlsdr/radio.js"; 
+import { modeParameters } from "@jtarrio/webrtlsdr/demod/modes.js"; 
 import { Demodulator } from "@jtarrio/webrtlsdr/demod/demodulator.js";
 import { RTL2832U_Provider } from "@jtarrio/webrtlsdr/rtlsdr.js"; 
 import { webusb } from "usb"; 
@@ -26,12 +27,12 @@ let demodulator = new Demodulator({
     }
 });
 
+const modeParams = modeParameters(demodulator.getMode());
+modeParams.setStereo(true);
+console.log(modeParams.getStereo());
 
 let radio = new Radio(new RTL2832U_Provider(), demodulator, {buffersPerSecond:20}); // by default buffersPerSecond : 20 (histoires de latence)
-// let radio = new Radio(new RtlProvider(), demodulator);
+
 radio.setFrequency(91.7e6);
 demodulator.setVolume(1);
 radio.start();
-
-
-// essayer avec la vraie antenne, sans globalThis, puis sur la raspberry en changeant le nombre de tappsssss
--- v3/basicTest3_0_4.js
+++ v3/basicTest3_0_4.js
@@ -1,13 +1,15 @@
 import { Radio, RtlProvider } from "@jtarrio/webrtlsdr/radio.js"; 
+import { modeParameters } from "@jtarrio/signals/demod/modes.js"; 
+import { RTL2832U_Provider } from "@jtarrio/webrtlsdr/rtlsdr.js";
 import { Demodulator } from "@jtarrio/signals/demod/demodulator.js";
+import { AudioPlayer } from "@jtarrio/signals/players/audioplayer.js";
 import { webusb } from "usb"; 
 import { AudioContext } from 'isomorphic-web-audio-api';
-globalThis.AudioContext = AudioContext;
+
 globalThis.navigator.usb = webusb;
 
-
 let demodulator = new Demodulator({
-    modeOption: {
+	modeOption: {
   /**
    * The time constant for the deemphasizer, in microseconds. 50 by default.
    *
@@ -21,14 +23,26 @@ let demodulator = new Demodulator({
   /** Number of taps for the audio filter. Must be an odd number. 41 by default. */
   audioTaps: 41
 },
+    player: new AudioPlayer({newAudioContext: () => new AudioContext()})
 });
 
-console.log(new RtlProvider())
 
-let radio = new Radio(new RtlProvider(), demodulator, { bufferPerSecond : 20 });  // by default buffersPerSecond : 20 (histoires de latence)
+let rtlProvider = new RtlProvider();
+
+let radio = new Radio(
+    rtlProvider, 
+    demodulator, 
+    { bufferPerSecond: 20 }
+);
+
+// console.log();
+const modeParams = modeParameters(demodulator.getMode());
+
+modeParams.setStereo(true);
+console.log(modeParams.getStereo());
+
 radio.setFrequency(91.7e6);
 demodulator.setVolume(1);
 radio.start();
 
 
-// essayer avec la vraie antenne, sans globalThis, puis sur la raspberry en changeant le nombre de tappsssss
